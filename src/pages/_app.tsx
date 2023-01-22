fee42283b3906bd8f617e47bfabb4a1eebd5491c
Ruben E Acosta <eldragondera1@gmail.com>
2023-01-22 16:54:33 -0500

    next ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, NextUIProvider } from '@nextui-org/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = createTheme({
+    type: "dark", // it could be "light" or "dark"
+    theme: {
+      colors: {
+        primary: '#24d754',
+        secondary: '#f78741',
+        error: '#e84f4f',
+      },
+    }
+  })
+
+  return (
+    <NextUIProvider theme={theme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@nextui-org/react"
 import { NextPage } from "next"
 
 const Home: NextPage = () => {
   return (
     <>
       <h1>Hola bebe</h1>
+      <Button>
+        Hola
+      </Button>
     </>
   )
 }
