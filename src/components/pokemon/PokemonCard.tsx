8a7a40b52b070c83033bc7a9c843cc2bf9a3f421
Ruben E Acosta <eldragondera1@gmail.com>
2023-01-23 21:27:20 -0500

    detalle pokemon

--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -1,5 +1,6 @@
 import { SmallPokemon } from '@/interfaces'
 import { Grid, Card, Row, Text } from '@nextui-org/react'
+import { useRouter } from 'next/router';
 import { FC } from 'react'
 
 type Props = {
@@ -8,15 +9,22 @@ type Props = {
 
 export const PokemonCard: FC<Props> = ({ pokemon }) => {
 
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push(`/pokemon/${pokemon.id}`)
+  }
+
   return (
     <Grid key={pokemon.id} xs={6} sm={3} md={2} xl={1}>
-      <Card isHoverable>
+      <Card isHoverable css={{ cursor: 'pointer' }}>
         <Card.Body css={{ p:1 }}>
           <Card.Image
             src={pokemon.img}
             alt={pokemon.name}
             width='100%'
             height={140}
+            onClick={handleClick}
           />
         </Card.Body>
         <Card.Footer>
